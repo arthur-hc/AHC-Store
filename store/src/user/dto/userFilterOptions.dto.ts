7a1cd918131c5ae7c340d2d72b8280ce99f054ff
arthur-hc <arthurhermannc@gmail.com>
2024-11-16 14:18:23 -0300

    feat: userFilterOptionsDto

---
+++ store/src/user/dto/userFilterOptions.dto.ts
@@ -0,0 +1,15 @@
+import { IsOptional, IsString } from 'class-validator';
+
+export class UserFilterOptionsDto {
+  @IsString()
+  @IsOptional()
+  id?: string;
+
+  @IsString()
+  @IsOptional()
+  name?: string;
+
+  @IsString()
+  @IsOptional()
+  email?: string;
+}
