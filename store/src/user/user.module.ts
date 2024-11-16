30b3b12243fd0037b584c5d88753d7d17364e08c
arthur-hc <arthurhermannc@gmail.com>
2024-11-16 20:16:16 -0300

    feat: add entity in modules

--- store/src/user/user.module.ts
+++ store/src/user/user.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
 import { UserRepository } from './user.repository';
 import { UserService } from './user.service';
 import { UniqueEmailValidator } from './validation/uniqueEmailValidator';
+import { User } from './entities/user.entity';
 
 @Module({
   controllers: [UserController],
-  imports: [],
+  imports: [TypeOrmModule.forFeature([User])],
   providers: [UserRepository, UserService, UniqueEmailValidator],
 })
 export class UserModule {}
