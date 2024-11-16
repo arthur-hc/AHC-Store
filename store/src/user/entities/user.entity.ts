908f6e9324113fbaa81dc03ec664a81deda9b038
arthur-hc <arthurhermannc@gmail.com>
2024-11-16 10:28:59 -0300

    feat: add  postgre config to user entity

--- store/src/user/entities/user.entity.ts
+++ store/src/user/entities/user.entity.ts
@@ -1,13 +1,25 @@
-export class UserEntity {
-  constructor(name: string, email: string, password: string, id?: string) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-    this.password = password;
-  }
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from 'typeorm';
 
+@Entity({ name: 'user' })
+export class UserEntity {
+  @PrimaryColumn({ name: 'id', type: 'uuid' })
   id?: string;
+
+  @Column({ name: 'name', length: 100, nullable: false })
   name: string;
+
+  @Column({ name: 'email', length: 70, nullable: false })
   email: string;
+
+  @Column({ name: 'password', length: 70, nullable: false })
   password: string;
+
+  @CreateDateColumn({ name: 'create_at' })
+  createAt?: Date;
+
+  @CreateDateColumn({ name: 'update_at' })
+  updateAt?: Date;
+
+  @CreateDateColumn({ name: 'delete_at' })
+  deleteAt?: Date;
 }
